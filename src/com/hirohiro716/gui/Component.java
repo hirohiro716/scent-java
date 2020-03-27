d858fe89367d1f8a8ee68fcb9d30830c4785bb13
hiro <hiro@nova>
2020-03-27 15:43:48 +0900

    Added method

--- src/com/hirohiro716/gui/Component.java
+++ src/com/hirohiro716/gui/Component.java
@@ -382,6 +382,18 @@ public abstract class Component<T extends java.awt.Component> {
         this.setMaximumSize(this.getMaximumWidth(), height);
     }
     
+    /**
+     * 最大サイズ、最小サイズをセットして、このコンポーネントのサイズを固定する。
+     * 
+     * @param width
+     * @param height
+     */
+    public void setFixedSize(int width, int height) {
+        this.setMinimumSize(width, height);
+        this.setMaximumSize(width, height);
+        this.setSize(width, height);
+    }
+    
     private Bounds bounds = new Bounds();
     
     /**
