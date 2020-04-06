0ddd891232092d7460b92deded289503d1fecced
hiro <hiro@nova>
2020-04-06 13:00:58 +0900

    Fixed bug

--- src/com/hirohiro716/database/Database.java
+++ src/com/hirohiro716/database/Database.java
@@ -280,6 +280,7 @@ public abstract class Database implements Closeable {
         } catch (SQLException exception) {
             throw exception;
         } catch (Exception exception) {
+            exception.printStackTrace();
         }
         throw new DataNotFoundException();
     }
