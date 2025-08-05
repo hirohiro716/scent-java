25cc48fff9c60be93504ffdb661b4bc60e08e07e
hiro <hirohiro716@gmail.com>
2025-08-05 11:32:34 +0900

    Add methods for unit conversion

--- src/main/java/com/hirohiro716/scent/datetime/Span.java
+++ src/main/java/com/hirohiro716/scent/datetime/Span.java
@@ -42,6 +42,51 @@ public class Span {
         return this.endDatetime;
     }
 
+    /**
+     * ミリ秒数で取得する。
+     * 
+     * @returns
+     */
+    public long toMilliseconds() {
+        return this.endDatetime.getAllMilliSecond() - this.startDatetime.getAllMilliSecond();
+    }
+
+    /**
+     * 秒数で取得する。
+     * 
+     * @returns
+     */
+    public double toSeconds() {
+        return this.toMilliseconds() / 1000d;
+    }
+    
+    /**
+     * 分数で取得する。
+     * 
+     * @returns
+     */
+    public double toMinutes() {
+        return this.toMilliseconds() / 1000d / 60d;
+    }
+
+    /**
+     * 時間数で取得する。
+     * 
+     * @returns
+     */
+    public double toHours() {
+        return this.toMilliseconds() / 1000d / 60d / 60d;
+    }
+
+    /**
+     * 日数で取得する。
+     * 
+     * @returns
+     */
+    public double toDays() {
+        return this.toMilliseconds() / 1000d / 60d / 60d / 24d;
+    }
+
     @Override
     public String toString() {
         return StringObject.joinWithSeparator(new Object[] {this.startDatetime.toString(), this.endDatetime.toString()}, " - ").toString();
